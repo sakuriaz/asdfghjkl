e7b5aaa6a5374587318397b9da05c17053f0582a
sakuriaz <80486447+sakuriaz@users.noreply.github.com>
2021-08-26 22:38:23 +0200

    Update startup.cmd

--- startup.cmd
+++ startup.cmd
@@ -1,2 +1,2 @@
 PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1
-PowerShell C:\Users\7uzp\Nedlastinger\asdfghjkl.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
+PowerShell C:\Users\7uzp\Desktop\asdfghjkl.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
